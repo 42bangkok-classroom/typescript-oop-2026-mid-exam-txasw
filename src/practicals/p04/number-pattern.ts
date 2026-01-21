1e94bca6b14d4dacb52951e26cf2ebac01618eb4
txasw <txasw@outlook.co.th>
2026-01-21 14:12:25 +0700

    prac: p04 - 001

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,15 @@
 // Write your code below
+const rawPattern = process.argv.slice(2)
+const pattern = Number(rawPattern)
+
+if (pattern > 0) {
+    for (let i = 1; i <= pattern; i++) {
+        for (let j = 0; j < pattern - i; j++) {
+            process.stdout.write(" ")
+        }
+        for (let k = i; k >= 1; k--) {
+            process.stdout.write(`${k}`)
+        }
+        console.log()
+    }
+}
