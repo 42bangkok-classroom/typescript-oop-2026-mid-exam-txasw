9f6e95ff26381695c31d659362c69629785a06c1
txasw <txasw@outlook.co.th>
2026-01-21 15:08:13 +0700

    prac: p02 - 002

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -2,15 +2,15 @@
 const rawGrade = process.argv.slice(2)
 const grade = Number(rawGrade)
 if (grade >= 0 && grade <= 49) {
-    console.log("F")
+    console.log("Grade is F")
 } else if (grade >= 0 && grade <= 59) {
-    console.log("D")
+    console.log("Grade is D")
 } else if (grade >= 0 && grade <= 69) {
-    console.log("C")
+    console.log("Grade is C")
 } else if (grade >= 0 && grade <= 79) {
-    console.log("B")
+    console.log("Grade is B")
 } else if (grade >= 0 && grade <= 100) {
-    console.log("A")
+    console.log("Grade is A")
 } else {
     console.log("Invalid input")
 }
