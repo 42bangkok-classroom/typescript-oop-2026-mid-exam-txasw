9a3eaa844120feb68eccc91c5ca94eef8e581920
txasw <txasw@outlook.co.th>
2026-01-21 16:46:06 +0700

    prac: p08 - 000 (test)

--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -1,6 +1,36 @@
-interface FullAlbum {}
+import axios from "axios";
+
+interface FullAlbum {
+    userId: number,
+    id: number,
+    title: string,
+    photos: Photo[]
+}
+
+type Photo = {
+    albumId: number,
+    id: number,
+    title: string,
+    url: string
+    thumbnailUrl: string,
+}
+
+type Album = {
+    userId: number,
+    id: number,
+    title: string,
+}
 
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
 
-function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]>
+async function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+    if (!userIds) return []
+    const photos = (await axios.get(PHOTOS_URL)).data as Photo[]
+    const albums = (await axios.get(ALBUMS_URL)).data as Album[]
+    
+
+    return []
+}
+
+mapPhotoToAlbum([])
