bca11809ae09f9bc90576f7fcdc95e34c0ebf2d4
txasw <txasw@outlook.co.th>
2026-01-21 17:04:39 +0700

    prac: p09 - 005

--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 export function readJson(fileName: string): unknown[] {
     let data
+    throw fileName
     try {
         data = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' })
     }catch{
