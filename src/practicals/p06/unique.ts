b82c9fff30e65a56561bf9e9a550c02e6af7ae4e
txasw <txasw@outlook.co.th>
2026-01-21 15:15:20 +0700

    prac: p06 - 003

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -2,7 +2,7 @@ export function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   // Write your code below
   const unq1 = arr1.filter(val => arr2.indexOf(val) == -1)
   const unq2 = arr2.filter(val => arr1.indexOf(val) == -1)
-  return unq1.concat(unq2)
+  return unq1.concat(unq2).flat()
 }
 
 const arr1 = [1, 2, 3, 4];
