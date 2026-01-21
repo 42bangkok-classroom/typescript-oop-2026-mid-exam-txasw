349edbc7b92432860eafd18e6f10e2ae7b605b04
txasw <txasw@outlook.co.th>
2026-01-21 14:04:26 +0700

    prac: 03 - 001

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,21 @@
 // Write your code below
+const rawFizz = process.argv.slice(2)
+const fizz = Number(rawFizz)
+for (let i = 1; i <= fizz; i++) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("FizzBuzz")
+        continue
+    }
+    
+    if (i % 3 == 0) {
+        console.log("Fizz")
+        continue
+    }
+
+    if (i % 5 == 0) {
+        console.log("Buzz")
+        continue
+    }
+
+    console.log(i)
+}
