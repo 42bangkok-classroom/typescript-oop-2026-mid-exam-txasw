1c8afe067fb423c739964201c52b792a4cdac77f
txasw <txasw@outlook.co.th>
2026-01-21 14:17:07 +0700

    prac: 05 - 001

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -2,4 +2,5 @@ const comment = "TypeScript is a strongly typed programming language"
 
 function shortenComment(comment: string): string {
   // Write your code below
+  return comment.split(" ").filter(word => word.length >= 5 && word.length <= 10).join(" ")
 }
