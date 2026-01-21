941b9ffe31c2d5a7283b808f88c87ca8ad403102
txasw <txasw@outlook.co.th>
2026-01-21 16:28:06 +0700

    prac: p09 - 001

--- src/practicals/p09/find-data.ts
+++ src/practicals/p09/find-data.ts
@@ -1,3 +1,16 @@
-function findData(fileName: string, key: string, value: string | number): void {
-  
+import { readJson } from "./read-json";
+import fs from 'fs';
+
+export function findData(fileName: string, key: string, value: string | number): void {
+  const data = readJson(fileName)
+  const findkey = data.filter((val: any) => val[key])
+  if (findkey.length === 0) throw "Invalid Key"
+
+  const foundPair = data.filter((val: any) => val[key] == value)
+  fs.writeFile(
+    "output.json",
+    JSON.stringify(foundPair),
+    err => {},
+    ); 
+
 }
--- src/practicals/p09/read-json.ts
+++ src/practicals/p09/read-json.ts
@@ -1,3 +1,15 @@
-function readJson(fileName: string): unknown[] {
-  
+export function readJson(fileName: string): unknown[] {
+    let data
+    try {
+        data = require(fileName)
+    }catch{
+        throw "Invalid file"
+    }
+    if (typeof data == 'object') return data
+
+    try {
+        return JSON.parse(data);
+    } catch {
+        throw "Invalid JSON format"
+    }
 }
